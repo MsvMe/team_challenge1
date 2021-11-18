4dc74823f107d86282e2865da6ceb85435d47c8c
Umar <mister.umars@mail.ru>
2021-11-18 11:36:32 +0300

    0 release, залил минимальный сервер

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+const PORT = 4000
+
+app.use(express.json())
+app.use(require('./routes/index'))
+
+const connect = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://admin:admin@cluster0.xd5ax.mongodb.net/Forum?retryWrites=true&w=majority"
+    )
+    console.log("Сервер подключен к базе данных")
+    app.listen(PORT, () => {
+      console.log(`Сервер запущен на порте ${PORT}...`)
+    })
+  } catch (e) {
+    console.log(e.message)
+  }
+}
+
+connect()
